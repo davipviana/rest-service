2f1f3fce708b6763bd8296b9b5da6272cf07fdb8
Davi Viana <davipviana@gmail.com>
2018-03-12 19:41:51 -0300

    Adicionados métodos da tela de login e registro.

--- app/src/main/java/com/davipviana/restservice/LoginRegisterActivity.java
+++ app/src/main/java/com/davipviana/restservice/LoginRegisterActivity.java
@@ -2,12 +2,78 @@ package com.davipviana.restservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class LoginRegisterActivity extends AppCompatActivity {
+    private UserLoginRegisterTask userLoginRegisterTask = null;
+    private EditText emailView;
+    private EditText passwordView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+        initViews();
+    }
+
+    private void initViews() {
+        emailView = (EditText) findViewById(R.id.email);
+        passwordView = (EditText) findViewById(R.id.password);
+    }
+
+    public void attemptLoginRegister(View view) {
+        if(userLoginRegisterTask != null) {
+            return;
+        }
+
+        emailView.setError(null);
+        passwordView.setError(null);
+
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if(!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordView.setError(getString(R.string.error_password_lenght));
+            focusView = passwordView;
+            cancel = true;
+        }
+        if(TextUtils.isEmpty(email)) {
+            emailView.setError(getString(R.string.error_field_required));
+            focusView = emailView;
+            cancel = true;
+        } else if(!isEmailValid(email)) {
+            emailView.setError(getString(R.string.error_invalid_email));
+            focusView = emailView;
+            cancel = true;
+        }
+
+        if(cancel) {
+            focusView.requestFocus();
+        } else {
+            userLoginRegisterTask = new UserLoginRegisterTask(email, password, view.getId() == R.id.email_sign_in_button);
+            userLoginRegisterTask.execute((Void) null);
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    private boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void showProgress(final boolean isShow) {
+        findViewById(R.id.login_progress).setVisibility(isShow ? View.VISIBLE : View.GONE);
+        findViewById(R.id.login_form).setVisibility(isShow ? View.GONE : View.VISIBLE);
     }
 }
