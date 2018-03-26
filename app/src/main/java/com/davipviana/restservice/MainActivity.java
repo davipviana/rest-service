e14adb2c872dc6429648d6d96a1dd349fb6bf0dd
Davi Viana <davipviana@gmail.com>
2018-03-26 11:31:54 -0300

    Adicionados métodos chamados no clique dos botões das ações de usuário.

--- app/src/main/java/com/davipviana/restservice/MainActivity.java
+++ app/src/main/java/com/davipviana/restservice/MainActivity.java
@@ -1,10 +1,14 @@
 package com.davipviana.restservice;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.davipviana.restservice.data.User;
 import com.davipviana.restservice.webservices.WebServiceTask;
@@ -60,6 +64,76 @@ public class MainActivity extends AppCompatActivity {
         nameText.setText(user.getName() == null ? "" : user.getName());
         phoneNumberText.setText(user.getPhoneNumber() == null ? "" : user.getPhoneNumber());
         noteText.setText(user.getNote() == null ? "" : user.getNote());
+    }
+
+    public void clickUpdateButton(View view) {
+        if(passwordText.getText().toString().trim().length() >= 5) {
+            showProgress(true);
+            userEditTask = new UserEditTask();
+            userEditTask.execute();
+        } else {
+            Toast.makeText(this, R.string.error_password_lenght, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void clickDeleteButton(View view) {
+        showConfirmationDialog(new ConfirmationListener(){
+            @Override
+            public void onConfirmation(boolean isConfirmed) {
+                if(isConfirmed) {
+                    showProgress(true);
+                    userDeleteTask = new UserDeleteTask();
+                    userDeleteTask.execute();
+                }
+            }
+        });
+    }
+
+    public void clickResetButton(View view) {
+        showConfirmationDialog(new ConfirmationListener() {
+            @Override
+            public void onConfirmation(boolean isConfirmed) {
+                if(isConfirmed) {
+                    showProgress(true);
+                    userResetTask = new UserResetTask();
+                    userResetTask.execute();
+                }
+            }
+        });
+    }
+
+    public void clickSignOutButton(View view) {
+        showLoginScreen();
+    }
+
+    private void showLoginScreen() {
+        Intent intent = new Intent(MainActivity.this, LoginRegisterActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void showConfirmationDialog(final ConfirmationListener confirmationListener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Are you sure? This operation cannot be undone.");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                confirmationListener.onConfirmation(true);
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                confirmationListener.onConfirmation(false);
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(true);
+        alertDialog.show();
     }
 
     private abstract class ActivityWebServiceTask extends WebServiceTask {
